b8275da08283b66a12e3a7b2480e2eada6addf3f
thinkGrow <rubaitreshad@gmail.com>
2024-11-26 05:47:56 +0600

    Add Conditional Rendering to ListGroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,12 @@
 function ListGroup() {
-  const items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+  items = [];
 
   return (
     // wrapped in fragment
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
