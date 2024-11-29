028b307495a7298142fc28e215298098617d35c1
thinkGrow <rubaitreshad@gmail.com>
2024-11-29 11:44:01 +0600

    Exercise : Build a button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import Alert from "./components/Alert";
+// import Alert from "./components/Alert";
+import Button from "./components/Button";
 
+// function App() {
+//   return (
+//     <div>
+//       <Alert>
+//         Hello <span>World</span> World
+//       </Alert>
+//     </div>
+//   );
+// }
 function App() {
   return (
     <div>
-      <Alert>Hello World</Alert>
+      <Button onClick={()=>console.log("Clicked")}>button</Button>
     </div>
   );
 }
